7b6d5f3f3294ab6b5f00374fb73abe73690c8f0c
mekmonroe92 <65523090+mekmonroe92@users.noreply.github.com>
2020-06-12 15:06:28 -0400

    Update browser manipulation.py

--- browser manipulation.py
+++ browser manipulation.py
@@ -1,6 +1,6 @@
 # using selenium to open browser/input information
 from selenium import webdriver
-browser = webdriver.Chrome('C:\\Users\monroem\PycharmProjects\chromedriver.exe')
+browser = webdriver.Chrome('path')
 type(browser)
 browser.get('https://www.facebook.com/')
 email = browser.find_element_by_id('email')
